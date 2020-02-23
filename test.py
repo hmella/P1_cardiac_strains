ff4bd261d6ce91df44f9c1ae3a277def1666684e
hmella <hmella@uc.cl>
2020-02-23 15:50:25 -0300

    Add functions to generate 2D data

---
+++ test.py
@@ -0,0 +1,58 @@
+import argparse
+
+import numpy as np
+
+from utils.generation import generate_cspamm, generate_dense, generate_phantoms
+
+##################
+# ARGS PARSER
+##################
+
+# Initiate the parser
+parser = argparse.ArgumentParser()
+
+# add long and short argument
+parser.add_argument("--generate-params", "-gp", help="Choose if paramters are generated or not", type=bool)
+parser.add_argument("--nb-data", "-Nd", help="Number of data", type=int)
+parser.add_argument("--nb-samples", "-Ns", help="Number of spins samples", type=int)
+
+# read arguments from the command line
+args = parser.parse_args()
+
+##################
+# INPUT PARAMETERS
+##################
+
+# Number of data and spins samples
+nb_data = args.nb_data
+nb_samples = args.nb_samples
+
+# Patients array
+patients = np.ones([nb_data,],dtype=np.bool)
+patients[0:int(patients.size/2)] = False 
+
+# Resolutions
+resolutions = [np.array([33, 33, 1]),    # pixel_size = 3.0  [mm]
+               np.array([40, 40, 1]),    # pixel_size = 2.5  [mm]
+               np.array([50, 50, 1]),    # pixel_size = 2.0  [mm]
+               np.array([67, 67, 1]),    # pixel_size = 1.5  [mm]
+               np.array([100, 100, 1])]  # pixel_size = 1.0  [mm]
+
+# Encoding frequencies
+tag_spacings = [0.0080, 0.0100, 0.0120, 0.0140, 0.0160] # tag spacings [m]
+cfrequencies = [np.array([2*np.pi/l,2*np.pi/l,0]) for l in tag_spacings]        # encoding frequency [rad/m]
+dfrequencies = [0.12*1000*2*np.pi*np.array([1,1,0])]                      # encoding frequency [rad/m]
+
+##################
+# DATA GENERATION
+##################
+
+# Generate phantoms
+if args.generate_params:
+    generate_phantoms(nb_samples,nb_data)
+
+# # Generate CSPAMM
+# generate_cspamm(resolutions, cfrequencies, patients, nb_data)
+
+# # Generate DENSE
+# generate_cspamm(resolutions, dfrequencies, patients, nb_data)
---
+++ utils/generation.py
@@ -0,0 +1,134 @@
+import os
+
+from PyMRStrain import *
+
+
+# Parameters generation
+def generate_phantoms(nb_samples,nb_data):
+
+    # Create folders
+    if not os.path.isdir('inputs/'):
+        os.mkdir('inputs/')
+    if not os.path.isdir('inputs/parameters'):
+        os.mkdir('inputs/parameters')
+    if not os.path.isdir('inputs/spins'):
+        os.mkdir('inputs/spins')
+
+    for d in range(nb_data):
+
+        # Create parameters
+        p = Parameters(time_steps=18)
+
+        # Create spins
+        s = Spins(Nb_samples=nb_samples,parameters=p)
+
+        # Write parameters and spins
+        save_pyobject(p,'inputs/parameters/p_{:03d}.pkl'.format(d))
+        save_pyobject(s,'inputs/spins/spins_{:03d}.pkl'.format(d), sep_proc=True)
+
+
+# CSPAMM images generation
+def generate_cspamm(resolutions, frequencies, patients, nb_data):
+
+    # Create folder
+    if not os.path.isdir('inputs/kspaces'):
+        os.mkdir('inputs/kspaces')
+
+    # Resolutions loop
+    for (rn, r) in enumerate(resolutions):
+
+        # Create image
+        I = CSPAMMImage(FOV=np.array([0.1, 0.1, 0.008]),
+                  center=np.array([0.0,0.0,0.0]),
+                  resolution=r,
+                  encoding_frequency=np.array([0,0,0]),
+                  T1=0.85,
+                  flip_angle=15*np.pi/180,
+                  encoding_angle=90*np.pi/180,
+                  kspace_factor=15,
+                  slice_thickness=0.008,
+                  oversampling_factor=1,
+                  phase_profiles=r[1])
+
+        # Frequencies loop
+        for (fn, f) in enumerate(frequencies):
+
+            # Set encoding frequency
+            I.encoding_frequency = f
+         
+            # Data loop
+            for d in range(nb_data):
+
+                # Load spins and parameter objects
+                spins = load_pyobject('inputs/spins/spins_{:03d}.pkl'.format(d),sep_proc=True)
+                param = load_pyobject('inputs/parameters/p_{:03d}.pkl'.format(d))
+
+                # Create phantom
+                phantom = Phantom(spins, param, patient=patients[d], z_motion=False)
+
+                # Artifact
+                artifact = None
+
+                # Generate kspaces
+                NSA_1, NSA_2, REF, mask = I.generate(artifact, phantom, param, debug=False)
+
+                # Compress kspaces
+                NSA_1.scale()
+                NSA_2.scale()
+
+                # Export kspaces
+                save_pyobject(NSA_1,'inputs/kspaces/Ck0_{:03d}_{:02d}_{:02d}.pkl'.format(d,fn,rn))
+                save_pyobject(NSA_2,'inputs/kspaces/Ck1_{:03d}_{:02d}_{:02d}.pkl'.format(d,fn,rn))
+
+
+# DENSE images generation
+def generate_dense(resolutions, frequencies, patients, nb_data):
+
+    # Create folder
+    if not os.path.isdir('inputs/kspaces'):
+        os.mkdir('inputs/kspaces')
+
+    # Resolutions loop
+    for (rn, r) in enumerate(resolutions):
+
+        # Create image
+        I = DENSEImage(FOV=np.array([0.1, 0.1, 0.008]),
+                  center=np.array([0.0,0.0,0.0]),
+                  resolution=r,
+                  encoding_frequency=np.array([0,0,0]),
+                  T1=0.85,
+                  flip_angle=15*np.pi/180,
+                  kspace_factor=15,
+                  slice_thickness=0.008,
+                  oversampling_factor=1,
+                  phase_profiles=r[1])
+
+        # Frequencies loop
+        for (fn, f) in enumerate(frequencies):
+
+            # Set encoding frequency
+            I.encoding_frequency = f
+         
+            # Data loop
+            for d in range(nb_data):
+
+                # Load spins and parameter objects
+                spins = load_pyobject('inputs/spins/spins_{:03d}.pkl'.format(d),sep_proc=True)
+                param = load_pyobject('inputs/parameters/p_{:03d}.pkl'.format(d))
+
+                # Create phantom
+                phantom = Phantom(spins, param, patient=patients[d], z_motion=False)
+
+                # Artifact
+                artifact = None
+
+                # Generate kspaces
+                NSA_1, NSA_2, REF, mask = I.generate(artifact, phantom, param, debug=False)
+
+                # Compress kspaces
+                NSA_1.scale()
+                NSA_2.scale()
+
+                # Export kspaces
+                save_pyobject(NSA_1,'inputs/kspaces/Dk0_{:03d}_{:02d}_{:02d}.pkl'.format(d,fn,rn))
+                save_pyobject(NSA_2,'inputs/kspaces/Dk1_{:03d}_{:02d}_{:02d}.pkl'.format(d,fn,rn))
