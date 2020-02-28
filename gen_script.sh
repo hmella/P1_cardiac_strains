d1474921ccd13b26b905ce509db9f47d56c59fd4
hernanmella <hmella@uc.cl>
2020-02-28 12:49:02 -0300

    Update script

--- gen_script.sh
+++ gen_script.sh
@@ -3,5 +3,5 @@ step=$1
 data=$2
 for ((i=0; i<=$((data-step)); i=$((i+step))))
   do
-     screen -d -m mpirun --allow-run-as-root -n 4 python3 test_base_generation.py -gp 1 -Nd $data -Ns 4000000 -nf 1 -ini $i -fin $((i + step))
+     screen -d -m mpirun --allow-run-as-root -n 4 python3 test_base_generation.py -gp -C -D -Nd $data -Ns 1000000 -nfree -i $i -f $((i + step))
  done
